faa775a2c2c19e94e19e7d513f481a0945f79f24
thejonathanr <jonathan.reiterman@gmail.com>
2015-12-30 16:56:06 -0800

    Added and took out some stuff! main.js is not useful currently.

--- main.js
+++ main.js
@@ -2,63 +2,61 @@
 Syndication Press
 **/
 
+function u(thing) { return document.getElementById(thing); }
+
 Game = {};
 
-Game.launch = function()
-{
+Game.money = 0;
+Game.jobs = 0;
+Game.staff = 1;
 
-	Game.money = 0;
-	Game.jobs = 0;
-	Game.staff = 1;
+Game.version = "0.0.1";
 
-	Game.version = "0.0.1";
+Game.companyName = "Your Company Name Here.";
 
-	Game.companyName = "Your Company Name Here.";
+u("name").innerHTML = Game.companyName;
 
-	/**Seasons and Holidays
+/**Seasons and Holidays
 
-	Holidays:
-	0 = no holiday
-	1 = New Years Day
-	2 = 
+Holidays:
+0 = no holiday
+1 = New Years Day
+2 = 
 
-	Seasons:
-	0 = Winter
-	1 = Spring
-	2 = Summer
-	3 = Fall
+Seasons:
+0 = Winter
+1 = Spring
+2 = Summer
+3 = Fall
 
 	
-	Date = {};
+Date = {};
 
-	Date.month = getMonth();
-	Date.day = getDate();
+Date.month = getMonth();
+Date.day = getDate();
 
-	if (Date.month === 0 && Date.day === 1) {
-		Date.holiday = 1;
-	}**/
+if (Date.month === 0 && Date.day === 1) {
+	Date.holiday = 1;
+}**/
 
-	Items = [];
-	Item = function(name,desc,image,price)
-	{
-		this.name = name;
-		this.desc = desc;
-		this.image = image;
-		this.price = price;
+Items = [];
+Item = function(name,desc,image,price)
+{
+	this.name = name;
+	this.desc = desc;
+	this.image = image;
+	this.price = price;
 
-		Items[name]=this;
-	}
-
-	new Item("Basic newspaper press","A basic newspaper press for your basic needs.","BasicPress.png",100);
-	new Item("Newspaper press","A reagular newspaper press.","",250);
-	new Item("Advanced newspaper Press","A advanced newspaper press for projects that\nrequire a lot of printing, ex:newspapers","",1000);
+	Items[name]=this;
 }
 
-Game.launch();
+new Item("Basic newspaper press","A basic newspaper press for your basic needs.","BasicPress.png",100);
+new Item("Newspaper press","A reagular newspaper press.","",250);
+new Item("Advanced newspaper Press","A advanced newspaper press for projects that\nrequire a lot of printing, ex:newspapers","",1000);
 
-Game.changeCompanyName = function()
-{
-	Game.companyName = prompt("New Company Name", Game.companyName);
+Game.changeCompanyName = function () {
+    Game.companyName = prompt("New Company Name", Game.companyName);
+    u("name").innerHTML = Game.companyName;
 }
 Game.addMoney = function(amount)
 {
